ee33acb26fc2b1f3f3654684dc4e79ec1b0b8267
Devbox <devbox@example.com>
2026-07-24 07:27:23 +0000

    Fix crash: use AndroidX MultiDex instead of old support library

--- android/app/src/main/java/com/gaoqianleme/app/MainApplication.java
+++ android/app/src/main/java/com/gaoqianleme/app/MainApplication.java
@@ -1,8 +1,8 @@
 package com.gaoqianleme.app;
 
 import android.content.Context;
-import android.support.multidex.MultiDex;
-import android.support.multidex.MultiDexApplication;
+import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
 import android.util.Log;
 
 public class MainApplication extends MultiDexApplication {
