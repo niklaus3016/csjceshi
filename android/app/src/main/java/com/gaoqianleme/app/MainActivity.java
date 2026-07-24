9bac884636ce104d6414d891d7afb11f087b19ae
Devbox <devbox@example.com>
2026-07-24 08:10:40 +0000

    Restore performRiskCheckFromFrontend method in MainActivity

--- android/app/src/main/java/com/gaoqianleme/app/MainActivity.java
+++ android/app/src/main/java/com/gaoqianleme/app/MainActivity.java
@@ -1,5 +1,15 @@
 package com.gaoqianleme.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    
+    public void performRiskCheckFromFrontend() {
+        RiskDetector.RiskResult result = RiskDetector.checkAllRisks(this);
+        if (result.hasRisk) {
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(0);
+        }
+    }
+}
