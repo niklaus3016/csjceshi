5ccd88a263434603da5adc2ec6abd4f80f807d29
Devbox <devbox@example.com>
2026-07-30 04:32:18 +0000

    chore: revert app id to 5858423, update icon to qxjf512, refresh site config

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -568,7 +568,7 @@ const loadWithdrawStatus = async () => {
 const adConfig = {
   appId: '5858423',
   slotIds: [
-    '104302392'   // 保价1500
+    '104282400'   // 保价1500
   ], // 按优先级从高到低排列
 };
 
